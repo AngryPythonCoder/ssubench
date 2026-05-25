98b977fbcabf412491860b0d08467288f5366ec1
AngryPythonCoder <angry.py.prog@gmail.com>
2026-05-25 19:15:54 +0000

    add timeouts and graceful shutdown

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"ssubench/internal/config"
 	"ssubench/internal/domain"
 	"ssubench/internal/handler"
 	"ssubench/internal/middleware"
 	"ssubench/internal/repository"
 	"ssubench/internal/service"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	chimiddleware "github.com/go-chi/chi/v5/middleware"
@@ -17,13 +21,22 @@ import (
 )
 
 func main() {
+	var exitCode int
+	defer func() {
+		if exitCode != 0 {
+			os.Exit(exitCode)
+		}
+	}()
+
 	cfg, err := config.Load()
 
 	ctx := context.Background()
 
 	db, err := repository.NewPGXConnection(ctx, cfg.DBURL)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		exitCode = 1
+		return
 	}
 
 	defer db.Pool.Close()
@@ -61,6 +74,7 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(
+		chimiddleware.Timeout(60*time.Second),
 		chimiddleware.RequestID,
 		chimiddleware.Logger,
 		chimiddleware.Recoverer,
@@ -110,6 +124,38 @@ func main() {
 		})
 	})
 
-	log.Println("Running on port " + cfg.ServerPort)
-	http.ListenAndServe(":"+cfg.ServerPort, r)
+	server := &http.Server{
+		Addr:         ":" + cfg.ServerPort,
+		Handler:      r,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		log.Println("Server is running on port " + cfg.ServerPort)
+
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Printf("Server error: %v\n", err)
+		}
+	}()
+
+	<-stop
+	log.Println("Shutting down the server")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = server.Shutdown(shutdownCtx)
+	if err != nil {
+		log.Printf("Server cannot shut down correctly: %v\n", err)
+		exitCode = 1
+		return
+	}
+
+	log.Println("Server stopped")
 }
